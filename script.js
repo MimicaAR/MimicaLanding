a6e016dc95b4e9b447a39143c2f973ea7583cad0
DESKTOP-5NSJLHS\chery <cheryboom100@gmail.com>
2017-09-12 20:41:20 +0300

    Second block finished. Phone movement logic simplified.

--- script.js
+++ script.js
@@ -18,50 +18,26 @@ window.addEventListener('load', function () {
 
     var phone = document.querySelector('.block--phone img'),
         offsetTop = phone.offsetTop;
-    var bodyRect = document.body.getBoundingClientRect(),
-        finalPos = document.querySelector('#project-items').offsetTop + document.querySelector('#project-items').clientHeight,
-        phoneRect = phone.getBoundingClientRect(),
-        offset   = phoneRect.top - bodyRect.top,
-        topPhone = offset - 85,
-        lastScrollPos = window.pageYOffset,
-        finished = false;
-
-    console.log(finalPos);
+    var finalPos = document.querySelector('#project-items').offsetTop + document.querySelector('#project-items').clientHeight,
+        phoneRect = phone.getBoundingClientRect();
 
     document.addEventListener('scroll', function () {
-        var finishRect = document.querySelector('#project-items').getBoundingClientRect(),
-            finish = finishRect.bottom - 1, // так надо
-            currentScrollPos = window.pageYOffset,
-            isScrollingDown = (currentScrollPos - lastScrollPos >= 0);
-        if (finished && !(isScrollingDown) && phone.getBoundingClientRect().top > 85) {
-            console.log(1);
-            phone.style.position = 'fixed';
-            phone.style.left = phoneRect.left + "px";
-            phone.style.top = "85px";
-            finished = false;
-        }
-        else if (finish < phone.getBoundingClientRect().bottom || window.pageYOffset >= finalPos - phone.clientHeight - 85) {
-            console.log(2);
-            console.log(phoneRect.bottom);
-            phone.style.position = 'relative';
+        var currentScrollPos = window.pageYOffset;
+
+        if (currentScrollPos < offsetTop - 85) {
+            phone.style.position = "relative";
+            phone.style.top = 0;
             phone.style.left = 0;
-            phone.style.top = finalPos - phone.clientHeight - offsetTop + 'px';
-            console.log(finalPos - phone.clientHeight - 85 + 'px');
-            finished = true;
         }
-        else if (currentScrollPos > topPhone && !(finished)) {
-            console.log(3);
-            phone.style.position = 'fixed';
-            phone.style.left = phoneRect.left + "px";
-            phone.style.top = "85px";
+        else if (currentScrollPos > finalPos - phone.clientHeight - 85) {
+            phone.style.position = "relative";
+            phone.style.top = finalPos - phone.clientHeight - offsetTop + "px";
+            phone.style.left = 0;
         }
         else {
-            phone.style.position = 'relative';
-            phone.style.left = 0;
-            phone.style.top = 0;
+            phone.style.position = "fixed";
+            phone.style.top = 85 + "px";
+            phone.style.left = phoneRect.left + "px";
         }
-
-        lastScrollPos = currentScrollPos;
-
     })
 })
